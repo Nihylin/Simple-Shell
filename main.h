6ec17cd642c4d2de65d5ed575ac5f617702e7a0d
Nihylin <nihylin@gmail.com>
2023-12-21 11:14:22 +0100

    Update main.c and create main.h & function.c files

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <ctype.h>
+
+#endif
